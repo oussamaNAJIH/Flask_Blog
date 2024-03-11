1ec1b8f2524b51689373c3c89441a8a41d6aa356
oussamaNAJIH <oussamanajih6@gmail.com>
2024-03-11 22:13:39 +0100

    forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '8efd9e88043c4d6161c29c7dca758fb6'
 
 posts = [
     {
@@ -26,12 +28,31 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello_world():
+def home():
     return render_template("home.html", posts=posts)
 
 @app.route("/about")
 def about():
     return render_template("about.html", title="about")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"account created for {form.username.data}", category="success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@gmail.com" and form.password.data == "password":
+            flash(f"You have been log in successfuly !", category="success")
+            return redirect(url_for("home"))
+        else:
+            flash(f"Login unsuccessful. Please check username and password", category="danger")
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == "__main__":
