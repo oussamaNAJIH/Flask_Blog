124733a15229a63bb1fe4f72e4bada88d95bcc52
oussamaNAJIH <oussamanajih6@gmail.com>
2024-03-13 00:02:52 +0100

    Package Structure

--- flaskblog/create_db.py
+++
@@ -1,7 +0,0 @@
-from flaskblog import app, db  # Import your Flask app instance and SQLAlchemy object
-from flaskblog import User, Post  # Import your SQLAlchemy models
-
-# Create all database tables
-with app.app_context():
-    db.drop_all()
-    db.create_all()
