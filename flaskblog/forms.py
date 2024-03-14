be2a3f39b56307b1fdc37f0706b09d47d816e2b3
oussamaNAJIH <oussamanajih6@gmail.com>
2024-03-14 23:34:45 +0100

    User Account and Profile Picture

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, file_allowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User, Post
+from flask_login import current_user
 
 
 class RegistrationForm(FlaskForm):
@@ -27,3 +29,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Log In')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update your profile picture', validators=[file_allowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username_to_check):
+        if current_user.username != username_to_check.data:
+            user = User.query.filter_by(username=username_to_check.data).first()
+            if user:
+                raise ValidationError("That username is taken. Please choose a different one.")
+        
+    def validate_email(self, email_to_check):
+        if current_user.email != email_to_check.data:
+            user = User.query.filter_by(email=email_to_check.data).first()
+            if user:
+                raise ValidationError("That email is taken. Please choose a different one.")
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,8 @@
+import os
+import secrets
+from flask import current_app
 from flask import render_template, url_for, flash, redirect, request
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flaskblog import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
@@ -73,7 +76,34 @@ def logout():
     logout_user()
     return redirect(url_for("home"))
 
-@app.route("/account")
+
+
+def save_pic(form_pic):
+    random_hex = secrets.token_hex(6)
+    _, file_ext = os.path.splitext(form_pic.filename)  # Extract filename from FileStorage object
+    picture_name = random_hex + file_ext
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics', picture_name)
+    form_pic.save(picture_path)
+    
+    return picture_name
+
+
+
+@app.route("/account", methods=["GET", "POST"])
 @login_required
 def account():
-    return render_template("account.html", title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture:
+            picture_file = save_pic(form.picture.data)
+            current_user.image_file = picture_file
+            db.session.commit()
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated", category="success")
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    return render_template("account.html", title="Account", image_file=image_file, form=form)
