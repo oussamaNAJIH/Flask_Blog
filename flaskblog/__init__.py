81b1ed638019a50fbaa29ccfe18757f8c449b31b
oussamaNAJIH <oussamanajih6@gmail.com>
2024-03-21 00:53:55 +0100

    reset password

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,6 +1,7 @@
 """
 The file where we initiate our application
 """
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
@@ -19,8 +20,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'blog3036@gmail.com'
-app.config['MAIL_PASSWORD'] = 'xuccxcnjzovgfqoz'
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
 
 mail = Mail(app)
 
